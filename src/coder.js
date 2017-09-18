99ec1034a18b30286028e64d3d94fdee5a06083f
Vyacheslav Pytel <dereckvk@gmail.com>
2017-09-18 23:33:02 +0300

    Fix null/undefined values

--- src/coder.js
+++ src/coder.js
@@ -4,8 +4,11 @@ module.exports = {
   encode: data => {
     let text = ''
     for (let x of data) {
-      for (let [ key, value ] of Object.entries(x))
+      for (let [ key, value ] of Object.entries(x)) {
+        if (value == null) continue
         text += key.replace(/\n/g, ' ') + ':' + value.toString().replace(/[\n|]/g, ' ') + '|'
+      }
+
       text += EOL
     }
     return text
